7d4253695c5bf91d265112e8d5b325d9f55cdcea
henry <henry.argueta@aol.com>
2019-10-25 20:44:37 -0400

    Updates

---
+++ precache-manifest.0fd10451116cd4791d14263c0144a412.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83b19a4d7245556cb4544a4b5ba2958",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "44bfc32cb64b1154de9e",
+    "url": "/weather/static/css/2.cf692049.chunk.css"
+  },
+  {
+    "revision": "2edbbc5dffada5697db5",
+    "url": "/weather/static/css/main.bce979ec.chunk.css"
+  },
+  {
+    "revision": "44bfc32cb64b1154de9e",
+    "url": "/weather/static/js/2.3394a52b.chunk.js"
+  },
+  {
+    "revision": "2edbbc5dffada5697db5",
+    "url": "/weather/static/js/main.f2cd3722.chunk.js"
+  },
+  {
+    "revision": "450fbd7864f155edc303",
+    "url": "/weather/static/js/runtime-main.de668264.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weather/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weather/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weather/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weather/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weather/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
